108a0799a68975c9f770a40d8f119b79ca20dd00
Ben Pringle <ben.pringle@gmail.com>
2012-11-19 19:21:50 -0500

    Add multithreading support

--- javaSpy/Main.java
+++ javaSpy/Main.java
@@ -5,27 +5,48 @@ class Main
     public static void main (String[] args)  throws java.lang.Exception
    {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String progName = in.readLine();
-        SysCommandExecutor systemDude = new SysCommandExecutor("/home/maksimv/Desktop/javaSpy");
-        systemDude.runCommand("pidof " + progName);
-        Thread.sleep(500);
-        int pidChars = systemDude.getCommandOutput().length();
-        System.out.println( progName + " " + systemDude.getCommandOutput().length());        
-        if(pidChars > 0)
-        {
-            System.out.println(progName + " is running");
+        while(true) {
+            String progName = in.readLine().substring(4);
+            SysCommandExecutor systemDude = new SysCommandExecutor(".");
+            systemDude.runCommand("pidof " + progName);
+            Thread.sleep(500);
+            int pidChars = systemDude.getCommandOutput().length();
+            System.out.println( progName + " " + systemDude.getCommandOutput().length());        
+            if(pidChars > 0)
+            {
+                System.out.println(progName + " is running");
+            }
+            if(pidChars == 0)
+            {
+                System.out.println(progName + " is not running");
+            }
+            
+            Notifier notifier = new Notifier(progName, pidChars);
+            notifier.start();
+       }
+   } 
+   static class Notifier extends Thread {
+       String progName;
+       int pidChars;
+       public Notifier(String progName, int pidChars) {
+           this.progName = progName;
+           this.pidChars = pidChars;
         }
-        if(pidChars == 0)
-        {
-            System.out.println(progName + " is not running");
-        }
-        
+       public void run() {
+        SysCommandExecutor systemDude;
         int newPidChars;
         while(true)
         {
-            systemDude = new SysCommandExecutor("/home/maksimv/Desktop/javaSpy");
-            systemDude.runCommand("pidof " + progName);
-            Thread.sleep(500);
+            systemDude = new SysCommandExecutor(".");
+            try{
+                systemDude.runCommand("pidof " + progName);
+            } catch (Exception e) {
+                continue;
+            }
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+            }
             newPidChars = systemDude.getCommandOutput().length();
             if(newPidChars > 0 && pidChars == 0)
             {
@@ -39,8 +60,6 @@ class Main
             pidChars = newPidChars;
             
         }       
-    
-
-      
-   } 
+       }
+   }
 }  
